f59ca225c6529f239661b417eff36420945d3b36
Noa83 <audrey.bc83@gmail.com>
2017-04-19 11:19:02 +0200

    Mise en page de la page Donate_join(+css et routing.yml). Creation de la liste déroulante de choix sur page consultation (results), mise en place des requetes dans observationRepository pour récuperer les mailles selon l'oiseau choisi.

--- src/AppBundle/Controller/ResultsController.php
+++ src/AppBundle/Controller/ResultsController.php
@@ -15,7 +15,7 @@ class ResultsController extends Controller
     /**
      * @Route("/results", name="results")
      */
-    public function resultsAction()
+public function resultsAction(Request $request)
     {
         //liste de choix des oiseaux
         $birds = $this->getDoctrine()->getRepository('AppBundle:Birds')->getBirdsList();
--- src/AppBundle/Repository/ObservationRepository.php
+++ src/AppBundle/Repository/ObservationRepository.php
@@ -36,16 +36,16 @@ class ObservationRepository extends \Doctrine\ORM\EntityRepository
         $rsm = new ResultSetMapping($em);
 
         $rsm->addScalarResult('nom_maille', 'nomMaille');
-        $rsm->addScalarResult('geojson', 'geometry');
+        $rsm->addScalarResult('geojson', 'polygon');
 
         //Requete sans le validated true
         $query = $this->_em->createNativeQuery("SELECT k.nom_maille, st_asgeojson(k.polygon) as geojson FROM observation o,
- km10 k WHERE o.bird_id = '" . $birdId . "' AND o.km10maille_id = k.id", $rsm);
-
+ km10 k WHERE o.bird_id = '".$birdId."' AND o.km10maille_id = k.id", $rsm);
 
         //Requete avec le validated true
 //        $query = $this->_em->createNativeQuery("SELECT k.nom_maille, st_asgeojson(k.polygon) as geojson FROM observation o,
 // km10 k WHERE o.bird_id = '".$birdId."' AND o.km10maille_id = k.id AND o.validated = true", $rsm);
+
 
 
         $results = $query->getResult();
