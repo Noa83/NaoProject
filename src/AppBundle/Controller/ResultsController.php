3deacd7b4b68a721e93400dda1558bd411f2be85
Noa83 <audrey.bc83@gmail.com>
2017-04-19 11:19:02 +0200

    Mise en page de la page Donate_join(+css et routing.yml). Creation de la liste déroulante de choix sur page consultation (results), mise en place des requetes dans observationRepository pour récuperer les mailles selon l'oiseau choisi.

--- src/AppBundle/Controller/ResultsController.php
+++ src/AppBundle/Controller/ResultsController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Model\ResultsModel;
+use AppBundle\Form\Type\ResultsType;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class ResultsController extends Controller
@@ -12,8 +15,36 @@ class ResultsController extends Controller
     /**
      * @Route("/results", name="results")
      */
-    public function resultsAction()
+public function resultsAction(Request $request)
     {
-        return $this->render('Results/results.html.twig');
+        //liste de choix des oiseaux
+        $birds = $this->getDoctrine()->getRepository('AppBundle:Birds')->getBirdsList();
+
+        $resultsModel = new ResultsModel();
+        $resultsForm = $this->get('form.factory')->create(ResultsType::class,
+            $resultsModel, ['birdList' => $birds]);
+
+        //période concernée?
+
+        //validation du choix
+        if ($request->isMethod('POST') && $resultsForm->handleRequest($request)->isValid()){
+            dump($resultsModel);
+
+            $listResults = $this->getDoctrine()->getRepository('AppBundle:Observation')->getMailleGeoJsonByBird($resultsModel->bird);
+            //$listResults2 = $this->getDoctrine()->getRepository('AppBundle:Observation')->getObservationsInfosWithBirdInfo($resultsModel->bird);
+
+            dump($listResults);
+            //dump($listResults2);
+        }
+
+
+
+
+
+
+        return $this->render('Results/results.html.twig', [
+            'birds' => $birds,
+            'form' => $resultsForm
+                ->createView()]);
     }
 }
