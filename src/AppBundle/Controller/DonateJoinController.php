8bb1f33cb03f9ad531575d7f5c4f73a8ef6cc314
Noa83 <audrey.bc83@gmail.com>
2017-04-20 15:57:26 +0200

    ajustement  route sur DonateJoinController suite au merge.

--- src/AppBundle/Controller/DonateJoinController.php
+++ src/AppBundle/Controller/DonateJoinController.php
@@ -2,12 +2,15 @@
 
 namespace AppBundle\Controller;
 
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class DonateJoinController extends Controller
 {
+    /**
+     * @Route("/donate_join", name="donate_join")
+     */
     public function donateJoinAction()
     {
         return $this->render('Assoc/donate_join.html.twig');
