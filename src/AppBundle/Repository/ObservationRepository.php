e44f219e460a242474c6bd751332a9520f4aa92c
Noa83 <audrey.bc83@gmail.com>
2017-04-20 17:47:31 +0200

    suite mise en place de la requete Ajax pour le resultat geojson

--- src/AppBundle/Repository/ObservationRepository.php
+++ src/AppBundle/Repository/ObservationRepository.php
@@ -93,7 +93,6 @@ class ObservationRepository extends EntityRepository
             'type' => 'FeatureCollection',
             'features' => $feature
         );
-
         return $geojson;
     }
 }
