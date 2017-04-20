735fa3383d5d4d55b03b9995f1c4b0bc5f02c679
Noa83 <audrey.bc83@gmail.com>
2017-04-20 11:32:06 +0200

    Affinage requête de récupération des mailles selon l'oiseau séléctionné et transfo en une réponse géojson.

--- src/AppBundle/Controller/ResultsController.php
+++ src/AppBundle/Controller/ResultsController.php
@@ -23,6 +23,7 @@ class ResultsController extends Controller
         $resultsForm->handleRequest($request);
         if ($request->isMethod('POST') && $resultsForm->isSubmitted() && $resultsForm->isValid()) {
            return $this->redirectToRoute('results_list', ['birdId' => $resultsModel->birdId]);
+            //return $listResults;
         }
         return $this->render('Results/results.html.twig', [
             'birds' => $birds,
