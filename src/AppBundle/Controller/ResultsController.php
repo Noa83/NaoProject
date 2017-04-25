d745a2b413f60e4e5eba522b6a557bfd4fb3a379
Noa83 <audrey.bc83@gmail.com>
2017-04-25 14:27:13 +0200

    finalisation geoJson et requete associée DQL

--- src/AppBundle/Controller/ResultsController.php
+++ src/AppBundle/Controller/ResultsController.php
@@ -3,15 +3,11 @@
 namespace AppBundle\Controller;
 
 
-use AppBundle\AppBundle;
-use AppBundle\Entity\Km10;
-use AppBundle\Repository\Km10Repository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Model\ResultsModel;
 use AppBundle\Form\Type\ResultsType;
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -67,8 +63,6 @@ class ResultsController extends Controller
     public function getBirdsResultsAction($id)
     {
         return new Response ($this->get('data_to_geojson')->getGeoJson($this->getDoctrine()
-            ->getRepository('AppBundle:Observation')->getMailleGeoJsonByBird($id)));
+            ->getRepository('AppBundle:Observation')->getObservationInfoWithMailleByBird($id)));
     }
-
-
 }
