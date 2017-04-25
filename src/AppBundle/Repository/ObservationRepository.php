31d46aa1d5205627b3564e303e44479f87673600
Flow791 <Flow791@outlook.fr>
2017-04-25 17:05:15 +0200

    Apparition des observation OK - Fiche oiseau OK - Images à revoir

--- src/AppBundle/Repository/ObservationRepository.php
+++ src/AppBundle/Repository/ObservationRepository.php
@@ -78,4 +78,12 @@ class ObservationRepository extends EntityRepository
     {
 
     }
+
+    /**
+     *
+     */
+    public function find10ByBird($bird){
+
+            return $this->findBy(array("bird" => $bird), array('id' => 'DESC','date' => 'DESC'));
+    }
 }
