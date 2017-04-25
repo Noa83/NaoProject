1d6c99186379a592edab38740d12898476629141
Noa83 <audrey.bc83@gmail.com>
2017-04-25 14:27:13 +0200

    finalisation geoJson et requete associée DQL

--- src/AppBundle/Repository/ObservationRepository.php
+++ src/AppBundle/Repository/ObservationRepository.php
@@ -2,11 +2,7 @@
 
 namespace AppBundle\Repository;
 
-use AppBundle\Entity\Km10;
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Query\ResultSetMapping;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * ObservationRepository
@@ -16,65 +12,44 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class ObservationRepository extends EntityRepository
 {
-    public function getObservationsInfosWithBirdInfo($birdId)
+//    public function getObservationsInfosWithBirdInfo($birdId)
+//    {
+//        $list = $this->createQueryBuilder('o')
+//            ->where('o.bird = :birdId')
+//            ->setParameter('birdId', $birdId)
+////            ->andWhere('o.validated = true')
+//            ->leftJoin('o.km10Maille', 'km')
+//            ->addSelect('km')
+////            ->addGroupBy('o.km10Maille')
+//            ->getQuery()
+//            ->getResult();
+//        return $list;
+//    }
+
+    public function getObservationInfoWithMailleByBird($birdId)
     {
         $list = $this->createQueryBuilder('o')
             ->where('o.bird = :birdId')
             ->setParameter('birdId', $birdId)
 //            ->andWhere('o.validated = true')
-            ->leftJoin('o.bird', 'bird')
-            ->addSelect('bird')
+            ->leftJoin('o.km10Maille', 'km')
+            ->addSelect('km')
             ->getQuery()
             ->getResult();
-    }
+        return $list;
 
-    public function getMailleGeoJsonByBird($birdId)
-    {
-        $em = $this->getEntityManager();
-        $rsm = new ResultSetMapping($em);
-
-        $rsm->addEntityResult(Km10::class, 'k');
-        $rsm->addFieldResult('k', 'id', 'id');
-        $rsm->addFieldResult('k', 'nom_maille', 'nomMaille');
-        $rsm->addFieldResult('k', 'polygon', 'polygon');
-
-        //Requete sans le validated true
-        $query = $this->_em->createNativeQuery("SELECT k.nom_maille, ST_AsText(k.polygon) as polygon, k.id FROM observation o,
- km10 k WHERE o.bird_id = :birdId AND o.km10maille_id = k.id", $rsm)
-            ->setParameter('birdId',$birdId);
-
-
-        //Requete avec le validated true
-//        $query = $this->_em->createNativeQuery("SELECT k.nom_maille, ST_AsText(k.polygon) as polygon FROM observation o,
-// km10 k WHERE o.bird_id = :birdId AND o.km10maille_id = k.id AND o.validated = true", $rsm)
-//              ->setParameter('birdId',$birdId);
-
-        $results = $query->getResult();
-        return $results;
     }
 
     public function getNbBirdsByMailleForChoicedBird($birdId)
     {
-        $em = $this->getEntityManager();
-        $rsm = new ResultSetMapping($em);
-
-        $rsm->addEntityResult(Km10::class, 'k');
-        $rsm->addFieldResult('k', 'id', 'id');
-        $rsm->addFieldResult('k', 'nom_maille', 'nomMaille');
-        $rsm->addFieldResult('k', 'polygon', 'polygon');
-
-        //Requete sans le validated true
-        $query = $this->_em->createNativeQuery("SELECT k.nom_maille, ST_AsText(k.polygon) as polygon, k.id FROM observation o,
- km10 k WHERE o.bird_id = :birdId AND o.km10maille_id = k.id", $rsm)
-            ->setParameter('birdId',$birdId);
-
-        //Requete avec le validated true
-//        $query = $this->_em->createNativeQuery("SELECT k.nom_maille, ST_AsText(k.polygon) as polygon FROM observation o,
-// km10 k WHERE o.bird_id = :birdId AND o.km10maille_id = k.id AND o.validated = true", $rsm)
-//              ->setParameter('birdId',$birdId);
-
-        $results = $query->getResult();
-        return $results;
+        $list = $this->createQueryBuilder('o')
+            ->select('COUNT(DISTINCT o.km10Maille)')
+            ->where('o.bird = :birdId')
+            ->setParameter('birdId', $birdId)
+//            ->andWhere('o.validated = true')
+            ->getQuery()
+            ->getResult();
+        return $list;
     }
 
     public function getOneMailleGeoJsonByBird($birdId)
