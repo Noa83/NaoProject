675e2f3377369463b26c302adc76b0048d067bb6
Noa83 <audrey.bc83@gmail.com>
2017-04-25 17:11:56 +0200

    Créa de la requête de compte du nombre d'oiseau par maille, créa du service datatoArrayMailleName pour la mise en tableau exploitable des résultats.

--- src/AppBundle/Repository/ObservationRepository.php
+++ src/AppBundle/Repository/ObservationRepository.php
@@ -11,19 +11,6 @@ use Doctrine\ORM\EntityRepository;
 class ObservationRepository extends EntityRepository
 {
     public function getObservationInfoWithMailleByBird($birdId)
-//    {
-//        $list = $this->createQueryBuilder('o')
-//            ->where('o.bird = :birdId')
-//            ->setParameter('birdId', $birdId)
-////            ->andWhere('o.validated = true')
-//            ->leftJoin('o.km10Maille', 'km')
-//            ->addSelect('km')
-////            ->addGroupBy('o.km10Maille')
-//            ->getQuery()
-//            ->getResult();
-//        return $list;
-//    }
-
     public function getObservationInfoWithMailleByBird($birdId)
     {
         $list = $this->createQueryBuilder('o')
@@ -60,18 +47,6 @@ class ObservationRepository extends EntityRepository
             throw new \Exception();
         }
     }
-    }
-
-    public function getNbBirdsByMailleForChoicedBird($birdId)
-    {
-        $list = $this->createQueryBuilder('o')
-            ->select('COUNT(DISTINCT o.km10Maille)')
-            ->where('o.bird = :birdId')
-            ->setParameter('birdId', $birdId)
-//            ->andWhere('o.validated = true')
-            ->getQuery()
-            ->getResult();
-        return $list;
     }
 
     public function getOneMailleGeoJsonByBird($birdId)
