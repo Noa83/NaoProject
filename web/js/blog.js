99cf8bcd98794013595c108694f8983725294dee
bruno974 <gont.bruno@gmail.com>
2017-05-08 14:58:00 +0200

    Ajout de la fonction collante sur article récent

---
+++ web/js/blog.js
@@ -0,0 +1,37 @@
+$(function(){
+
+    var choix = 0;
+
+    $('.collant').each(function () {
+        var parent = $(this).parent();
+        var defaultTop = $(this).offset().top;
+        var elementCourant = $(this);
+        var largeur = $(window).width();
+
+        $(window).scroll(function () {
+            if(choix == 0)
+            {
+                if (largeur > 1024) {
+                    if ($(window).scrollTop() > defaultTop) {
+                        elementCourant.stop().animate({top: $(window).scrollTop()}, 500)
+                    }
+                    else {
+                        elementCourant.stop().animate({top: defaultTop - 110}, 500)
+                    }
+                }
+            }
+            else
+            {
+                if (largeur > 1024) {
+                    if ($(window).scrollTop() > defaultTop) {
+                        elementCourant.stop().animate({top: $(window).scrollTop() - 50}, 500)
+                    }
+                    else {
+                        elementCourant.stop().animate({top: defaultTop - 110}, 500)
+                    }
+                }
+            }
+
+        })
+    });
+});
